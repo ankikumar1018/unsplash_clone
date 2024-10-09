c1c6c0be6d7730373182c67bc0a58eca19edbbbd
Ankit Kumar <aktetri@gmail.com>
2024-10-09 09:02:39 +0530

    Resolved redirect issue

--- unsplash_clone/settings.py
+++ unsplash_clone/settings.py
@@ -135,5 +135,6 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'image_list'
+LOGOUT_REDIRECT_URL = 'login'
