729b29d522876bd55ab7766b1429db7e9aaae26f
Ankit Kumar <aktetri@gmail.com>
2024-10-09 09:06:20 +0530

    Resolved redirect issue (#1)

--- images/views.py
+++ images/views.py
@@ -1,15 +1,18 @@
+import random
 
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404, redirect
 from django.http import FileResponse
+from django.shortcuts import render, get_object_or_404, redirect
+
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+
 from .models import Image
 from .serializers import ImageSerializer
-import random
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, logout
 
 
 @login_required
@@ -45,18 +48,6 @@ def profile_view(request):
     return render(request, "profile.html")
 
 
-def register_view(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect("image_list")
-    else:
-        form = UserCreationForm()
-    return render(request, "register.html", {"form": form})
-
-
 def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
@@ -67,6 +58,19 @@ def login_view(request):
     else:
         form = AuthenticationForm()
     return render(request, "login.html", {"form": form})
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Account created for {username}!")
+            return redirect("image_list")
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form": form})
 
 
 def logout_view(request):
